386635f85a8df4b8f8b0cdc3e64f8d5085a3b323
Núria <ruizmunoznuria@gmail.com>
2025-12-03 19:11:41 +0100

    render

--- backend/PetConnect/PetConnect/settings.py
+++ backend/PetConnect/PetConnect/settings.py
@@ -65,6 +65,7 @@ LOGOUT_REDIRECT_URL = '/usuarios/login'
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
